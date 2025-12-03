3f9afb195be855df28e897eabe78cfa6cf937779
hansa <hansader8@gmail.com>
2025-12-03 11:13:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ClassComponent from './component/ClassComponent'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <ClassComponent/>
+    </>
+  )
+}
+
+export default App
